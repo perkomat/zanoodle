bab9fa05de3e79ffd2a36abd7ffca6f8e39aa0ed
perkomat <perko.matej1@gmail.com>
2026-02-15 10:15:48 +0100

    kt7
    ..

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- KONSTANTE ---
-const INITIAL_SPEED_MS = 1000; 
-const SPEED_INCREMENT_MS = 200; 
+const INITIAL_SPEED_MS = 600; 
+const SPEED_INCREMENT_MS = 40; 
 const SCORE_PER_FOOD = 1;      
 const FOOD_COUNT = 3; 
 
